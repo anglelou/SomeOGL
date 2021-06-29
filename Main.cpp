c3b3dbee52d1812107b6df411fe8bec4fecbf560
Angel Soto <angells825@gmail.com>
2021-06-29 15:01:45 -0700

    Added color to window

--- Main.cpp
+++ Main.cpp
@@ -4,20 +4,37 @@
 
 int main()
 {
+	// Initialize GLFW
 	glfwInit();
 
+	// Declare version of OpenGl 3.3 to be used 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// Declare use of CORE profile
+	// Meaning, we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "An OpenGL Window", NULL, NULL);
+	// Create a GLFWwindow object of 800 by 800 pixels 
+	GLFWwindow* window = glfwCreateWindow(800, 800, "A Window", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
+
+	// Load GLAD so it configures OpenGL
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 800);
+
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
 
 	while (!glfwWindowShouldClose(window))
 	{
